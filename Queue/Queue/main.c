f4a4f30769e1cb6a2aedd1c1140574114d5ea4b9
Niksen111 <nikiksen2003@gmail.com>
2021-10-12 15:17:02 +0300

    Added deQueue function

--- Queue/Queue/main.c
+++ Queue/Queue/main.c
@@ -10,8 +10,8 @@ typedef struct
 
 typedef struct
 {
-    struct QueueElement* head;
-    struct QueueElement* tail;
+    QueueElement* head;
+    QueueElement* tail;
 }Queue;
 
 Queue* createQueue(void)
@@ -26,7 +26,11 @@ void enQueue(Queue* queue, int value) //добавляет в хвост
 
 int deQueue(Queue* queue) // удаляет в голову
 {
-
+    int number = queue->head->value;
+    QueueElement* temporaryQueueElement = queue->head;
+    queue->head = queue->head->next;
+    free(temporaryQueueElement);
+    return number;
 }
 
 bool isEmpty(Queue* queue)
@@ -41,5 +45,10 @@ void deleteQueue(Queue* queue)
 
 int main()
 {
-
+    QueueElement* queueElement = NULL;
+    QueueElement queueElement1 = { .value = 10, .next = queueElement};
+    QueueElement queueElement2 = { .value = 20, .next = &queueElement1};
+    Queue* queue = malloc(sizeof(Queue));
+    queue->head = &queueElement2;
+    queue->tail = queueElement;
 }
